16a10260a2c75c15deec1116af9bb0adb45d3051
Raul Rotundo <raulrotundo@gmail.com>
2017-10-24 22:50:52 -0300

    Adding customer pagination

--- routes/customers.js
+++ routes/customers.js
@@ -5,10 +5,17 @@ var Customers = require('../models/customers');
 
 // Get all customers
 router.get('/', VerifyToken, (req, res, next) => {
-  Customers.find({}, function (err, customers) {
+  // Access the provided 'page' and 'items_per_page' query parameters  
+  var items_per_page = Number(req.query.items_per_page) || 10;
+  var page = req.query.page > 0 ? ((req.query.page - 1) * items_per_page) : 0;
+
+  Customers.find({}, {}, { skip: page, limit: items_per_page }, function (err, customers) {
     if (err) res.status(500).json(err);
 
     res.status(200).json({
+      total: customers.length,
+      page: page,
+      items_per_page: items_per_page,
       data: customers
     });
   });
