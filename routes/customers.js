024538c356496ed863d6d6ab31560255b19e61e2
Raul Rotundo <raulrotundo@gmail.com>
2017-10-25 23:01:27 -0300

    Adding query condition

--- routes/customers.js
+++ routes/customers.js
@@ -3,13 +3,21 @@ var router = express.Router();
 var VerifyToken = require('../middlewares/VerifyToken');
 var Customers = require('../models/customers');
 
-// Get all customers
+/**
+ * Get all customers
+ * API /api/customers
+ * 
+ * @param string items_per_page -> Items per page to be displayed
+ * @param string page -> Number of page to be displayed
+ * @param string q -> Query condition
+ */
 router.get('/', VerifyToken, (req, res, next) => {
   // Access the provided 'page' and 'items_per_page' query parameters  
   var items_per_page = Number(req.query.items_per_page) || 10;
   var page = req.query.page > 0 ? ((req.query.page - 1) * items_per_page) : 0;
+  var query = req.query.q ? { name: new RegExp(req.query.q, 'i') } : {};
 
-  Customers.find({}, {}, { skip: page, limit: items_per_page }, function (err, customers) {
+  Customers.find(query, {}, { skip: page, limit: items_per_page }, (err, customers) => {
     if (err) res.status(500).json(err);
 
     res.status(200).json({
