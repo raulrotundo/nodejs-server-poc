1d6a4e229b5cdc9ac14db2295ac7c61e519b45e2
Raul Rotundo <raulrotundo@gmail.com>
2017-10-29 18:10:56 -0300

    Getting products support using limit and offset parameter for pagination

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,31 @@ var router = express.Router();
 var VerifyToken = require('../middlewares/VerifyToken');
 var Products = require('../models/products');
 
-// Get all products
+/**
+ * Get all products
+ * API /api/products
+ * 
+ * @param string items_per_page -> Limit of items per page to be displayed
+ * @param string page -> Number of page to be displayed
+ * @param string q -> Query condition
+ */
 router.get('/', VerifyToken, (req, res, next) => {
-  Products.find({}, function (err, products) {
+  var limit = Number(req.query.items_per_page) || 10;
+  var offset = req.query.page > 0 ? ((req.query.page - 1) * limit) : 0;
+  var query = req.query.q ? { name: new RegExp(req.query.q, 'i') } : {};
+
+  Products.find(query, (err, products) => {
     if (err) res.status(500).json(err);
 
+    var data = products.slice(offset, limit + offset);
+    var total_pages = Math.ceil(products.length / limit);
+
     res.status(200).json({
-      data: products
+      total_pages: total_pages,
+      total_rows: products.length,
+      page: req.query.page,
+      items_per_page: limit,
+      data: data
     });
   });
 });
