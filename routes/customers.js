0b5ab3c6d27bdebb23d686c22117c3444c22b8fa
Raul Rotundo <rrotundo@digitalriver.com>
2017-10-29 13:13:16 -0300

    Get total number of rows when using LIMIT and OFFSET

--- routes/customers.js
+++ routes/customers.js
@@ -7,24 +7,27 @@ var Customers = require('../models/customers');
  * Get all customers
  * API /api/customers
  * 
- * @param string items_per_page -> Items per page to be displayed
+ * @param string items_per_page -> Limit of items per page to be displayed
  * @param string page -> Number of page to be displayed
  * @param string q -> Query condition
  */
 router.get('/', VerifyToken, (req, res, next) => {
-  // Access the provided 'page' and 'items_per_page' query parameters  
-  var items_per_page = Number(req.query.items_per_page) || 10;
-  var page = req.query.page > 0 ? ((req.query.page - 1) * items_per_page) : 0;
+  var limit = Number(req.query.items_per_page) || 10;
+  var offset = req.query.page > 0 ? ((req.query.page - 1) * limit) : 0;
   var query = req.query.q ? { name: new RegExp(req.query.q, 'i') } : {};
 
-  Customers.find(query, {}, { skip: page, limit: items_per_page }, (err, customers) => {
+  Customers.find(query, {}, {}, (err, customers) => {
     if (err) res.status(500).json(err);
 
+    var data = customers.slice(offset, limit + offset);
+    var total_pages = Math.ceil(customers.length / limit);
+
     res.status(200).json({
-      total: customers.length,
-      page: page,
-      items_per_page: items_per_page,
-      data: customers
+      total_pages: total_pages,
+      total_rows: customers.length,
+      page: req.query.page,
+      items_per_page: limit,
+      data: data
     });
   });
 });
